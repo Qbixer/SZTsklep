8e76e67fa916c2d55c17825dca9781ad651240c5
Jakub Falkowski <Qbixer@gmail.com>
2018-11-28 10:19:41 +0100

    Main views 2/3

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+appname = 'main'
+urlpatterns = [
+    path('', views.index, name='index'),
+
+    path('contact/', views.contact, name='contact'),
+]
