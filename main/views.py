d31899f9002bc952326e3443236c55345028df9c
Jakub Falkowski <Qbixer@gmail.com>
2018-11-28 10:45:08 +0100

    Done

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Price
 # Create your views here.
 def contact(request):
   return render(request, 'main/contact.html')
@@ -9,4 +10,8 @@ def index(request):
     return render(request, 'main/index.html')
 
 def prices(request):
-    return render(request,'main/prices.html')
+    prices = Price.objects.order_by('id')
+    context = {
+        'prices': prices
+    }
+    return render(request,'main/prices.html',context)
